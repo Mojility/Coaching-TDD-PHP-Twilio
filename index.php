6b7d1b5c10c8b24355f53d959728242b4ec2ac58
Stacey Vetzal <stacey@vetzal.com>
2016-03-22 18:08:57 -0400

    merging global shift

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-require_once("inc/Group.class.php");
+require_once("inc/globals.php");
+require_once("inc/Group.php");
+require_once("inc/ResponseBuilder.php");
 
 define("SCRIPT_URL", "http://myserver.com/phones/index.php");
 
