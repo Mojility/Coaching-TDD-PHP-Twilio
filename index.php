36a51878e6ef83ff1b29cd0d9efad032921c0e93
Stacey Vetzal <stacey@vetzal.com>
2016-03-22 09:21:11 -0400

    moved defines

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ require_once("inc/Group.class.php");
 
 define("SCRIPT_URL", "http://myserver.com/phones/index.php");
 
+define("FORWARD_MODE", "forward");
+define("REJECT_MODE", "reject_busy");
+define("ADMIN_PHONE", "+19055551212");
+define("ADMIN2_PHONE", "+14165551212");
+define("MEMBER_PHONE", "+17055551212");
+
 $From = $_POST['From'];
 //$From = ADMIN_PHONE;
 //$From = MEMBER_PHONE;
