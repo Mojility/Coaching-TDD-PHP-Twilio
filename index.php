9d4c8868de995970a3129f22bd0294f6bebce30c
Stacey Vetzal <stacey@vetzal.com>
2016-03-22 21:44:33 -0400

    tweak

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ require_once("inc/globals.php");
 require_once("inc/Group.php");
 require_once("inc/Dispatcher.php");
 
-$From = $_POST['From'];
+//$From = $_POST['From'];
 //$From = ADMIN_PHONE;
-//$From = MEMBER_PHONE;
+$From = MEMBER_PHONE;
 
-$Digits = $_POST['Digits'];
+//$Digits = $_POST['Digits'];
 //$Digits = "6135551212";
-//$Digits = "48424";
+$Digits = "48424";
 
 $group = new Group();
 $responseBuilder = new Dispatcher();
