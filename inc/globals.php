32e8ab4436e533127d44b5425a8847827386d257
Stacey Vetzal <stacey@vetzal.com>
2016-03-22 21:01:45 -0400

    moving globals around

--- inc/globals.php
+++ inc/globals.php
@@ -5,3 +5,5 @@ define("REJECT_MODE", "reject_busy");
 define("ADMIN_PHONE", "+19055551212");
 define("ADMIN2_PHONE", "+14165551212");
 define("MEMBER_PHONE", "+17055551212");
+
+define("SCRIPT_URL", "http://myserver.com/phones/index.php");
--- index.php
+++ index.php
@@ -3,14 +3,6 @@ require_once("inc/globals.php");
 require_once("inc/Group.php");
 require_once("inc/ResponseBuilder.php");
 
-define("SCRIPT_URL", "http://myserver.com/phones/index.php");
-
-define("FORWARD_MODE", "forward");
-define("REJECT_MODE", "reject_busy");
-define("ADMIN_PHONE", "+19055551212");
-define("ADMIN2_PHONE", "+14165551212");
-define("MEMBER_PHONE", "+17055551212");
-
 $From = $_POST['From'];
 //$From = ADMIN_PHONE;
 //$From = MEMBER_PHONE;
